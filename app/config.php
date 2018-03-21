03342499bd7bd6d11569dc6ca43963b44937b03e
Sam McGeown <sammcgeown@users.noreply.github.com>
2018-03-21 15:00:37 +0000

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_SERVER', 'DBServer');
+define('DB_USERNAME', 'DBUser');
+define('DB_PASSWORD', 'DBPassword');
+define('DB_NAME', 'DBName');
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
